64dd227eee7f5591f4e14ee0681b5840c4287242
Paula MacTavish <perilousp@hotmail.com>
2018-03-11 17:45:01 -0400

    latest

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,39 +1,41 @@
 $(document).ready(function () {
-
+// Global Variables
 var animal = "dog";
 var behavior = ["Tail", "Smile", "Lazy", "Swim", "Play", "Bathe", "Sleep", "Stairs", "Eat", "Lick", "Treats", "Fetch", "Car", "Cat"];
 var giphy;
 
-//for (var i=0; i < (animal.length); i++) {
+    // Make Buttons
     for (var j=0; j < (behavior.length); j++) {
         var dogButtons = $("<button>" + (behavior[j]) + "</button>");
         dogButtons.addClass("buttons").val(behavior[j]);
         dogButtons.css({
-        "background": "rgb(20, 75, 20)", "margin-top": "7px", "margin-right": "5px", "font-size": "14px", "font-family": "'Merriweather', serif", "color": "rgb(255, 255, 222)", "border-radius": "8px", "padding": "6", "min-width": "85px"
+            "background": "rgb(20, 75, 20)", "margin-top": "7px", "margin-right": "5px", "font-size": "14px", "font-family": "'Merriweather', serif", "color": "rgb(255, 255, 222)", "border-radius": "8px", "padding": "6", "min-width": "85px"
         });
         $("#dogButtons").append(dogButtons);
     }; 
-//};    
-  
-$(document).on("click", "button.buttons", function loadGiphyStills() { 
-//    if ((this.value) !== behaviorWanted) {
+    
+    // Activate button attributes and populate div with still images when clicked 
+    $(document).on("click", "button.buttons", function loadGiphyStills() { 
+    // clear div
     $("#giphys").empty();
+    // set up info for query
     behaviorWanted= this.value;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=dog+" + behaviorWanted + "&api_key=lN8bGeGk9m7SFABzGBkz44bTJrCWU1KH&limit=10&fixed_height=200";
-     
+    // get info from url 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-//        console.log(response);
-  
+    })
+    // log response
+    .then(function(response) {
+        console.log(response);
+        // show images on screen
         for (var i =0; i < (response.data.length); i++) {
             var giphy = $("<embed src=" + (response.data[i].images["fixed_height_still"]["url"]) + 
             " data-animate=" + (response.data[i].embed_url) + "  data-still=" + response.data[i].images["fixed_height_still"]["url"] + " data-state='still' Class='gifs'>");
             giphy.css({
-                "margin-top": "10px", "margin-right": "8px", "margin-bottom": "-4px", "border-color": "rgb(255, 255, 222)", "border": "solid", "border-width": "3px", "width": "200px", "height": "160px", 
-               // "overflow": "hidden"
-                });
+                "margin-top": "10px", "margin-right": "8px", "margin-bottom": "-4px", "border-color": "rgb(255, 255, 222)", "border": "solid", "border-width": "3px", "width": "200px", "height": "160px"
+            });
             // console.log(response.data[0].images["fixed_height_still"]["url"]);
             // console.log(response.data[i].embed_url);
             $("#giphys").append(giphy);
@@ -41,45 +43,29 @@ $(document).on("click", "button.buttons", function loadGiphyStills() {
     });
 });
 
+    // activate image attributes
+    $("#giphys").on("click", "embed.gifs", function animateGiphy(){   
+        var state = $(this).attr("data-state");
+        // check to see if the image is animated or still
+        if (state === "still") {
+            //if still, animate by chaning the src url and the data state to animate
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            console.log("Why Won't")
+        } else {
+            // if animated, stil by changing the src url and the data state to still
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            console.log("This Work????")
+        }
+    });
 
-$("#giphys").on("click", "embed.gifs", function animateGiphy(){
-    var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-        // (this).css({
-        //     "width": "500px", "height": "350px"
-        // })
-        console.log("HI")
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-        console.log("THERE")
-        // giphy.css({
-        //     "width": "200px", "height": "160px"
-        // })
-      }
-});
-
-$("#giphys").on("clickUp", "embed.gifs", function activateAnimateGiphy(){
-    console.log("HI")
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-          console.log("if state still")
-        // giphy.css({
-        // "width": "75%", "height": "60%"
-        // })
-      } else {
-        console.log("if state active") 
-        // giphy.css({
-        // "width": "200px", "height": "160px"
-        // })
-      }
-});
+    $("#giphys").on("clickUp", "embed.gifs", function activateAnimateGiphy(){
+        if (state === "still") {
+            console.log("I'm getting")
+        } else {
+            console.log("really FRUSTRATED") 
+        }
+    });
 
 });
